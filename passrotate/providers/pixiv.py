4bea6c9688b4d3c81f4891dbf68cd0e13cc0cc97
flan <flan@flande.re>
2017-03-03 19:06:57 +0100

    pixiv: Print unexpected login errors as well
    The path-based check is still useful in case the login API gets
    changed.

--- passrotate/providers/pixiv.py
+++ passrotate/providers/pixiv.py
@@ -30,7 +30,7 @@ class Pixiv(Provider):
             "password": old_password
         })
         r = self._session.post("https://accounts.pixiv.net/api/login",
-                data=self._form)
+                params={"lang": "en"}, data=self._form)
         json = r.json()
         if "body" in json and \
            "validation_errors" in json['body']:
@@ -38,6 +38,8 @@ class Pixiv(Provider):
                 raise Exception("Pixiv has locked us out of further login attempts. Try again later.")
             elif json['body']['validation_errors'].get("captcha"):
                 raise Exception("Failed to log into pixiv: captcha authentication required")
+            else:
+                raise Exception("Failed to log into pixiv: {}".format(json['body']['validation_errors']))
 
         r = self._session.get("https://www.pixiv.net/setting_userdata.php",
                 params={"type": "password"})
